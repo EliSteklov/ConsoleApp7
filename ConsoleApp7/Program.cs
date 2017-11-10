381b7effc77b0dd42587d457ba5178305519078b
Eli <Eli@STEKLOV>
2017-11-10 19:55:43 +0200

    LAst Change

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -14,6 +14,10 @@ namespace ConsoleApp7
 
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
