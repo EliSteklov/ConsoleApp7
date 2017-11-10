bb6facfcd97c84c003415e97c07b7a95311c6586
Eli <Eli@STEKLOV>
2017-11-10 18:11:12 +0200

    Commit 2

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -7,6 +7,10 @@ namespace ConsoleApp7
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
